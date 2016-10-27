13b2c340560b1dae27a20423115aa58ae5923568
MarianaPacheco <pachecoramirez.mariana@gmail.com>
2016-10-27 16:53:19 -0500

    Agregué atributo draggable

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -80,15 +80,22 @@ function guardar() {
 		enlaceDentroDiv.appendChild(divTextAreaButtonSpan);
 		enlaceDentroDiv.parentElement.replaceChild(divTextAreaButtonSpan, enlaceDentroDiv);
 
+		var container;
 		function validarTextAnadirText() {
+			container = document.querySelector('.container');
+			container.addEventListener('dragover', dragSobreContainer, false);
+			container.addEventListener('dragleave', dragSalioContainer, false);
+			container.addEventListener('drop', manejarDrop, false);
 			if ( areaTextoCambio.value == "") {
 				areaTextoCambio.setAttribute('placeholder', 'Ingresa texto tarjeta');
 				return false;
-				//si lo valida
+				//si lo valida2
 			}	
 
 		var tarjetaTextoDiv = document.createElement('div');
 			tarjetaTextoDiv.setAttribute('class', 'tarjetaTextoh3Style');
+			tarjetaTextoDiv.setAttribute('draggable', 'true');
+			tarjetaTextoDiv.setAttribute('id', 'tarjetaTextDiv');
 
 		var textoh3IncluirTarjeta = document.createElement('h3');
 			textoh3IncluirTarjeta.innerHTML=areaTextoCambio.value;
@@ -99,12 +106,39 @@ function guardar() {
 
 
 		areaTextoCambio.value = "";
-		} buttonDentroDivAnadir.onclick=validarTextAnadirText;
+
+		tarjetaTextoDiv.addEventListener('dragstart', dragIniciado, false);
+		tarjetaTextoDiv.addEventListener('dragend', dragFinalizado, false);
+
+		} 
+		buttonDentroDivAnadir.onclick=validarTextAnadirText;
+
 
 
 
 	} enlaceDentroDiv.onclick = changeTarjetVStextarea;
 
+	function dragIniciado(e){
+			this.style.backgroundColor = 'blue';
+	}
+
+	function manejarDrop (e){
+		e.preventDefault();
+	}
+
+	function dragSobreContainer(e){
+		e.preventDefault();
+		this.classList.add('over');
+		return false;
+	}
+
+	function dragSalioContainer(e){
+		this.classList.remove('over');
+	}
+
+	function dragFinalizado(e){
+		
+	}
 	
 
 
@@ -113,3 +147,7 @@ function guardar() {
 	//Comenzar en 0 el input de Inicio
 	createInput.value = "";
 }
+
+
+
+/*Referencia minuto 13 https://www.youtube.com/watch?v=2YCc29vmNOE*/
